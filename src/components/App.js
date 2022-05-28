6e825f019bc4152b9f105a83bed116b7548819d5
Lydia Wallace <lydia.wallace@outlook.com>
2022-05-28 15:53:00 +0100

    first tests added and location details added

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import "../styles/App.css";
+
+import React from "react";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
+
+function App(props) {
+  const { location } = props;
+  return (
+    <div className="App">
+      <LocationDetails city={location.city} country={location.country} />
+    </div>
+  );
+}
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
+
+export default App;
